e19fed51cf02aeddfa97f25494b90553e994f4c5
gutierrezjuanmanuel <gutierrezmanueljuan@outlook.com>
2023-07-28 03:16:00 +0000

    cambio

---
+++ main.js
@@ -0,0 +1,100 @@
+
+// comentario de linea
+
+/* 
+comentario
+de
+bloque
+*/
+
+//forma recomendada para declarar variables
+// let edad = 15;
+// let nombre = "andres";
+
+//forma no recomendada para declarar variables
+// var numero = 4;
+
+//constantes
+// const NUMERO = 5;
+
+// edad = 20;
+
+// camelCase => nombreBonitoGrande
+// snake_case => nombre_bonito_grande
+// PascalCase => NombreBonitoGrande
+
+// let numeroUno = 3;
+// let numeroDos = 5;
+
+// let resultadoSuma = numeroUno + numeroDos;
+// let resultadoResta = numeroUno - numeroDos;
+// let resultadoMultiplicacion = numeroUno * numeroDos;
+// let resultadoDivision = numeroUno / numeroDos;
+
+// console.log("Resultado suma", resultadoSuma);
+// console.log("Resultado resta", resultadoResta);
+// console.log("Resultado multiplicacion", resultadoMultiplicacion);
+// console.log("Resultado division", resultadoDivision);
+
+// let saludo = "Hola";
+// let nombre = "Camila";
+// let saludoCompleto = saludo + " " + nombre;
+
+// console.log(saludoCompleto);
+
+// let nombre = prompt("Ingrese su nombre");
+// alert(nombre);
+
+// let numero = 6;
+// let nombre = "andres";
+// console.log(typeof numero);
+// console.log(typeof nombre);
+
+//let numero = Number(prompt("Ingrese un numero"));
+//alert(numero + 5);
+//console.log("Este es un mensaje de prueba",numero +5);
+
+//let nombre = prompt("Ingrese su nombre");
+//let saludo= "Bienvenido " + nombre + "!";
+//alert (saludo);
+//console.log(saludo);
+
+//let numero =Number (prompt("Ingrese un numero"));
+//let numeroUno=2
+//let resultadoMultiplicacion = numeroUno * numero;
+//let string="El doble del numero ingresado es: ";
+//let muestra= string + resultadoMultiplicacion;
+//alert (muestra);
+//console.log(muestra);
+
+//let nombre=  prompt("Ingrese su nombre");
+//let apellido= prompt ("Ingrese su apellido");
+//let nombreApellido=nombre + " " + apellido;
+//alert (nombreApellido);
+//console.log(nombreApellido);
+
+
+//let pregunta= prompt("Vive en Argentina?");
+//if ((pregunta=="Si") || (pregunta=="SI") || (pregunta=="si")|| (pregunta=="sI"))
+//{
+//    alert("Puede acceder a la promoción");
+//}
+//else{
+//    alert("No puede acceder a la promoción");
+//}
+
+//conteo de 100 entradas
+//for (let i = 0; i <= 100; i++) {
+//    alert(i);
+//}
+
+//let asiento = Number(prompt("Indique el numero de asiento que desee"));
+//Repetimos con While hasta que el usuario ingresa "ESC"
+//do
+//{
+//    alert("Su asiento es el n°: "+ asiento);
+//    Volvemos a solicitar un dato. En la próxima iteración se evalúa si no es ESC.
+//    asiento = prompt("Indique el numero de asiento que desee");
+//}
+//while(asiento >0 && asiento<=100)
+
