32ec3b7c9d6df85c6415479bb22c70b4ef28a42e
gutierrezjuanmanuel <gutierrezmanueljuan@outlook.com>
2023-08-19 23:38:58 +0000

    cambio

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
-//conteo de 2 pedidos
-for (let i = 0; i <= 2; i++) {
+const productos = [
+      { id: 1, nombre: "bateria", precio: 330.000 },
+      { id: 2, nombre: "guitarra", precio: 120.000 },
+       { id: 3, nombre: "violín", precio: 110.000},
+      { id: 4, nombre: "bajo", precio: 170.000 },
+     ];
     
-}
-
-const numeroDeProducto = () => {
-let producto = parseInt(prompt("Indique el numero de producto que desee.Para salir escriba ESC ."));
-//Repetimos con While hasta que el usuario ingresa "ESC"
-do
-{
-    alert("Su prdocuto es el n°: "+ producto);
-   // Volvemos a solicitar un dato. En la próxima iteración se evalúa si no es ESC.
-   producto = prompt("Indique el numero de producto que desee");
-}
-while(producto >0 && producto<=2)
-}
-
-numeroDeProducto ();
+    let nombre = prompt("Ingrese el nombre del producto a  consultar.Para salir escriba ESC .");
+    
+    while (nombre != "ESC") {
+      //declaramos la variable que contendrá el producto en caso de encontrarlo
+      let producto;
+    
+      //recorremos el arreglo verificando si alguno de los productos cumple con el nombre
+      for (const item of productos) {
+        if (item.nombre === nombre) {
+          producto = item;
+        }
+      }
+    
+      if (producto) {
+        let mensaje = `
+          Nombre: ${producto.nombre}
+          Precio: $${producto.precio}
+        `;
+    
+        alert(mensaje);
+      } else {
+        alert("El producto no se encuentra disponible");
+      }
+    
+      nombre = prompt("Ingrese el nombre del producto a  consultar");
+    }
