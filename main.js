271afda15bff232ee66e71a0d2561d481df0a165
gutierrezjuanmanuel <gutierrezmanueljuan@outlook.com>
2023-09-18 00:21:38 +0000

    cambio

--- main.js
+++ main.js
@@ -1,33 +1,35 @@
-//class Producto {
-//  constructor(id, nombre, precio) {
-//    this.id = id;
-//    this.nombre = nombre;
-//    this.precio = precio;
-//  }
-//}
+class Producto {
+  constructor(id, nombre, precio) {
+    this.id = id;
+    this.nombre = nombre;
+    this.precio = precio;
+  }
+}
 
-//let contenedor = document.getElementById("contenedor");
-//contenedor.className = "rojo";
+let contenedor = document.getElementById("contenedor");
+contenedor.className = "rojo";
 //arreglo que va a contener los productos creados
-//const productos = [];
+const productos = [];
 
 //agregamos los productos creados por la clase Producto al arreglo
-//productos.push(new Producto(1, "guitarra", 180000));
-//productos.push(new Producto(2, "violin", 50000));
-//productos.push(new Producto(3, "piano", 1111111));
-//productos.push(new Producto(4, "saxofon", 1013526));
-//productos.push(new Producto(5, "trompeta", 160000));
-//productos.push(new Producto(6, "teclado", 60000));
+productos.push(new Producto(1, "guitarra", 180000));
+productos.push(new Producto(2, "violin", 50000));
+productos.push(new Producto(3, "piano", 1111111));
+productos.push(new Producto(4, "saxofon", 1013526));
+productos.push(new Producto(5, "trompeta", 160000));
+productos.push(new Producto(6, "teclado", 60000));
 
-//const guardarStorage = (clave, valor) => {
-//localStorage.setItem(clave, valor);
-//};
 
-//productos.forEach((item) => {
-//guardarStorage(item.id, JSON.stringify(item));
-//});
+const guardarStorage = (clave, valor) => {
+  localStorage.setItem(clave, valor);
+};
 
-//localStorage.setItem("carrito", JSON.stringify(productos));
+productos.forEach((item) => {
+  guardarStorage(item.id, JSON.stringify(item));
+});
+
+localStorage.setItem("carrito", JSON.stringify(productos));
+
 
 
 
@@ -50,20 +52,42 @@ if (carritoStorage) {
   document.body.append(div);
 }
 
+const agregarCarrito = (id) => {
+  let producto = productos.find((item) => item.id === id);
+  let mensaje = `
+     Id: ${producto.id}
+     Nombre: ${producto.nombre}
+     Precio: ${producto.precio}
+   `;
+
+  alert(mensaje);
+
+
+};
 
 //recorro el carrito y muestro en el dom los productos carrito
-carrito.forEach((item) => {
+fetch("/data.json")
+.then((response) => response.json())
+.then((data) => {
+  data.forEach((item) => {
   let div = document.createElement("div");
   div.innerHTML = `
-    <h2>ID: ${item.id}</h2>
+  <div class="rojo">
+     <h2>Id: ${item.id}</h2>
     <p>Nombre: ${item.nombre}</p>
-    <b>$${item.precio}</b>
-    <button> agregar </button>
+   <b>$${item.precio}</b>
+    <button id="boton${item.id}">Agregar al carrito</button>
+    </div>
   `;
 
   document.body.append(div);
+  let boton = document.getElementById(`boton${item.id}`);
+  boton.addEventListener("click", () => agregarCarrito(item.id));
 
 });
+});
+
+
 
 //elimino el storage, muestro el alert y recargo la pagina
 eliminar.addEventListener("click", () => {
@@ -86,40 +110,88 @@ eliminar.addEventListener("click", () => {
   });
 });
 
+//traemos el formulario
+let formulario = document.getElementById("formulario");
+// //funcion para mostrar los productos en la pagina
+const renderizar = (carrito) => {
+  //antes de mostrar los productos en la pagina borramos el contenedor
+  contenedor.innerHTML = "";
+  carrito.forEach((item) => {
+    let div = document.createElement("div");
+    div.innerHTML = `
+       <h2>Id: ${item.id}</h2>
+       <p>Nombre: ${item.nombre}</p>
+       <b>$${item.precio}</b>
+     `;
+    contenedor.append(div);
+  });
+};
+
+// //agregamos el evento submit al formulario
+formulario.addEventListener("submit", (e) => {
+  //evitamos que el formulario se recargue
+  e.preventDefault();
+
+  //   //traemos los inputs
+  let inputs = e.target.children;
+  let nombre = inputs[0].value;
+  let precio = inputs[1].value;
+
+  //   //agregamos el nuevo producto al carrito
+  Math.max(...carrito.map(item => item.id)) + 1
+  carrito.push({ id: carrito.length + 1, nombre, precio });
+
+  //   //mostramos el carrito en la pagina
+  renderizar(carrito);
+});
+
+//agregar.addEventListener("click", () => agregarCarrito.forEach((item) => {
+//let div = document.createElement("div");
+//div.innerHTML = `
+//<h2>ID: ${item.id}</h2>
+//<p>Nombre: ${item.nombre}</p>
+//<b>$${item.precio}</b>
+//`;
+//
+// document.body.append(div);
+//
+//}));
+
+
 //localStorage.clear();
 //location.reload();
 //alert("Carrito eliminado");
 
 
 //const traerProductos = () => {
- // return new Promise((resolve, reject) => {
-   // setTimeout(() => {
-    //  reject("")
-     // // resolve(DB);
-    //}, 3000);
-  //});
+// return new Promise((resolve, reject) => {
+// setTimeout(() => {
+//  reject("")
+// // resolve(DB);
+//}, 3000);
+//});
 //};
 ////////////////////////////////////////////////
 
 //traerProductos()
-  //.then((response) => {
-   // response.forEach((item) => {
-   //   let div = document.createElement("div");
-   //   div.innerHTML = `
-   // <h2>ID: ${item.id}</h2>
-   // <p>Nombre: ${item.nombre}</p>
-  //  <b>$${item.precio}</b>
- // `;
+//.then((response) => {
+// response.forEach((item) => {
+//   let div = document.createElement("div");
+//   div.innerHTML = `
+// <h2>ID: ${item.id}</h2>
+// <p>Nombre: ${item.nombre}</p>
+//  <b>$${item.precio}</b>
+// `;
 //
-    //  document.body.append(div);
-   // });
- // })
-  //.catch((error) => {
-    //let div = document.createElement("div");
-   // div.innerHTML = `No hay productos en el carrito
+//  document.body.append(div);
+// });
+// })
+//.catch((error) => {
+//let div = document.createElement("div");
+// div.innerHTML = `No hay productos en el carrito
 //${error}`;
 
-  //  document.body.append(div);
- // });
+//  document.body.append(div);
+// });
 //
 //
